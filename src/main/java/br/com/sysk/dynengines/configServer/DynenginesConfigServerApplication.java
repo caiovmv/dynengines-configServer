8b1cc54d8d74be20dab9fd12ade7279100700659
Caio Villela <caio.villela@sysk.com.br>
2016-06-06 00:44:12 -0300

    starting project

---
+++ src/main/java/br/com/sysk/dynengines/configServer/DynenginesConfigServerApplication.java
@@ -0,0 +1,12 @@
+package br.com.sysk.dynengines.configServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynenginesConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynenginesConfigServerApplication.class, args);
+	}
+}
